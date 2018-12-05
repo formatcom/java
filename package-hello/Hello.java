298bc88a089a832e004c91148e0533ec0a29b345
Vinicio Valbuena <formatcomvinicio@gmail.com>
2018-12-05 15:34:31 -0500

    package

---
+++ package-hello/Hello.java
@@ -0,0 +1,12 @@
+import graphics.Point2D;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        Point2D p = new Point2D(5, 10);
+
+        System.out.println(p);
+    }
+
+}
